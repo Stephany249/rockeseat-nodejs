ab5d7c723612681d79bd3ce63e3a313912545c4a
Stephany <tete.1818@hotmail.com>
2024-01-18 12:09:57 -0300

    implementing the routes

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,8 +1,15 @@
 import fastify from 'fastify'
 import { env } from './env'
 import { transactionsRoutes } from './routes/transactions'
+import cookie from '@fastify/cookie'
 
 const app = fastify()
+
+app.register(cookie)
+
+app.addHook('preHandler', async (request) => {
+  console.log(`[${request.method}] ${request.url}`)
+})
 
 app.register(transactionsRoutes, {
   prefix: '/transactions',
