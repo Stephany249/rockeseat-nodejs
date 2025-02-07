b8d6ff87d82b51996c05b4212f361958a732cc73
Stephany <tete.1818@hotmail.com>
2025-02-07 16:44:46 -0300

    Use case: List response comments

--- 04-clean-ddd/test/repositories/in-memory-answer-comments-repository.ts
+++ 04-clean-ddd/test/repositories/in-memory-answer-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
 
@@ -14,6 +15,12 @@ export class InMemoryAnswerCommentsRepository implements AnswerCommentsRepositor
     return answerComment
   }
 
+  async findManyByAnswerId(answerId: string, { page }: PaginationParams) {
+    const answerComments = this.answerComments
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20)
+    return answerComments
+  }
 
   async create(answerComment: AnswerComment) {
     this.answerComments.push(answerComment)
