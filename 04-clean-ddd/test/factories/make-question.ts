b31b54887929316e5a48e6fc99648fd3441cb4ec
Stephany <tete.1818@hotmail.com>
2025-02-07 11:52:43 -0300

    Generating dummy data

--- 04-clean-ddd/test/factories/make-question.ts
+++ 04-clean-ddd/test/factories/make-question.ts
@@ -1,14 +1,25 @@
-import { UniqueEntityID } from "@/core/entities/unique-entity-id";
-import { Question, QuestionProps } from "@/domain/forum/enterprise/entities/question";
-import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
+/* eslint-disable prettier/prettier */
+import { faker } from '@faker-js/faker'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorId: new UniqueEntityID(),
-    title: 'Example',
-    content: 'Example content',
-    ...override
-  })
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import {
+  Question,
+  QuestionProps,
+} from '@/domain/forum/enterprise/entities/question'
+
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID,
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityID(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
