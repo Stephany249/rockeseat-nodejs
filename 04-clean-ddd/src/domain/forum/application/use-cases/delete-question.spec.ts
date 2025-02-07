8ce266ec2a097ffa1522efe784485dcfe5807a5e
Stephany <tete.1818@hotmail.com>
2025-02-07 12:42:10 -0300

    Use case: Delete question

---
+++ 04-clean-ddd/src/domain/forum/application/use-cases/delete-question.spec.ts
@@ -0,0 +1,46 @@
+import { DeleteQuestionUseCase } from './delete-question'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { makeQuestion } from 'test/factories/make-question'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: DeleteQuestionUseCase
+
+describe('Delete Question By Id', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new DeleteQuestionUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('should be able to delete a question', async () => {
+    const newQuestion = makeQuestion({
+      authorId: new UniqueEntityID('any_id'),
+    }, new UniqueEntityID('question-1'))
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    await sut.execute({
+      authorId: 'any_id',
+      questionId: 'question-1',
+    })
+
+    expect(inMemoryQuestionsRepository.questions).toHaveLength(0)
+  })
+
+
+  it('should not be able to delete a question from another user', async () => {
+    const newQuestion = makeQuestion({
+      authorId: new UniqueEntityID('any_id'),
+    }, new UniqueEntityID('question-1'))
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+
+    expect(() => {
+      return sut.execute({
+        authorId: 'any',
+        questionId: 'question-1',
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
--- 04-clean-ddd/test/repositories/in-memory-questions-repository.ts
+++ 04-clean-ddd/test/repositories/in-memory-questions-repository.ts
@@ -1,11 +1,23 @@
-import { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository"
-import { Question } from "@/domain/forum/enterprise/entities/question"
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+import { Question } from '@/domain/forum/enterprise/entities/question'
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public questions: Question[] = []
 
-  async findBySlug(slug: string): Promise<Question | null> {
-    const question = this.questions.find((question) => question.slug.value === slug)
+  async findById(id: string) {
+    const question = this.questions.find((question) => question.id.toString() === id)
+
+    if (!question) {
+      return null
+    }
+
+    return question
+  }
+
+  async findBySlug(slug: string) {
+    const question = this.questions.find(
+      (question) => question.slug.value === slug,
+    )
 
     if (!question) {
       return null
@@ -16,5 +28,11 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 
   async create(question: Question) {
     this.questions.push(question)
+  }
+
+  async delete(question: Question) {
+    const index = this.questions.findIndex((q) => q.id === question.id)
+
+    this.questions.splice(index, 1)
   }
 }
