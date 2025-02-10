e5be43dbbabfc52d2972c65f08cd59fcd9a9160c
Stephany <tete.1818@hotmail.com>
2025-02-10 17:31:19 -0300

    Listening for a domain event

--- 04-clean-ddd/test/repositories/in-memory-answers-repository.ts
+++ 04-clean-ddd/test/repositories/in-memory-answers-repository.ts
@@ -1,3 +1,4 @@
+import { DomainEvents } from '@/core/events/domain-events'
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerAttachmentsRepository } from '@/domain/forum/application/repositories/answer-attachments-repository'
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
@@ -30,12 +31,16 @@ export class InMemoryAnswersRepository implements AnswersRepository {
 
   async create(answer: Answer) {
     this.answers.push(answer)
+
+    DomainEvents.dispatchEventsForAggregate(answer.id)
   }
 
   async save(answer: Answer) {
     const index = this.answers.findIndex((q) => q.id === answer.id)
 
     this.answers[index] = answer
+
+    DomainEvents.dispatchEventsForAggregate(answer.id)
   }
 
   async delete(answer: Answer) {
