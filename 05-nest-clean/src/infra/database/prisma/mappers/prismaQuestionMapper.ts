6402c44b4bbb9e2b99841234041ae65fa552efa6
Stephany <tete.1818@hotmail.com>
2025-02-12 15:13:18 -0300

    Decoupling layers in NestJS - Implementing Questions Repository

--- 05-nest-clean/src/infra/database/prisma/mappers/prismaQuestionMapper.ts
+++ 05-nest-clean/src/infra/database/prisma/mappers/prismaQuestionMapper.ts
@@ -1,4 +1,4 @@
-import { Question as PrismaQuestion } from '@prisma/client'
+import { Question as PrismaQuestion, Prisma } from '@prisma/client'
 
 import { UniqueEntityID } from '@/core/entities/uniqueEntityId'
 import { Question } from '@/domain/forum/enterprise/entities/question'
@@ -20,5 +20,18 @@ export class PrismaQuestionMapper {
       },
       new UniqueEntityID(raw.id),
     )
+  }
+
+  static toPrisma(question: Question): Prisma.QuestionUncheckedCreateInput {
+    return {
+      id: question.id.toString(),
+      authorId: question.authorId.toString(),
+      bestAnswerId: question.bestAnswerId?.toString(),
+      title: question.title,
+      content: question.content,
+      slug: question.slug.value,
+      createdAt: question.createdAt,
+      updatedAt: question.updatedAt,
+    }
   }
 }
