bed914fc5db6982c9513f38056e51f38521091b5
Stephany <tete.1818@hotmail.com>
2025-02-16 17:31:38 -0300

    Initial commit

---
+++ fastfeet/eslint.config.mjs
@@ -0,0 +1,63 @@
+// @ts-check
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import * as eslintPluginImport from 'eslint-plugin-import';
+
+export default tseslint.config(
+  {
+    ignores: ['eslint.config.mjs'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    plugins: {
+      'import': eslintPluginImport
+    },
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      ecmaVersion: 5,
+      sourceType: 'module',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'no-useless-constructor': 'off',
+      'no-new': 'off',
+      'prettier/prettier': ['error', {
+        endOfLine: 'auto',
+        semi: false,
+        singleQuote: true
+      }],
+      '@typescript-eslint/no-empty-object-type': 'off',
+      'quotes': [
+        'error',
+        'single'
+      ],
+      'semi': [
+        'error',
+        'never'
+      ],
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal'],
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        'newlines-between': 'always', 
+        }
+      ]
+    },
+  },
+);
