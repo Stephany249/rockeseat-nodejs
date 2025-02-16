54742d7cd6747246d67f9503697690c33040eef8
Stephany <tete.1818@hotmail.com>
2025-02-16 13:49:57 -0300

    Relationships and Uploads - Value Object: Question Details

--- 05-nest-clean/src/domain/forum/application/repositories/questionsRepository.ts
+++ 05-nest-clean/src/domain/forum/application/repositories/questionsRepository.ts
@@ -1,9 +1,11 @@
 import { Question } from '../../enterprise/entities/question'
+import { QuestionDetails } from '../../enterprise/entities/value-objects/questionDetails'
 import { PaginationParams } from '@/core/repositories/paginationParams'
 
 export abstract class QuestionsRepository {
   abstract findById(id: string): Promise<Question | null>
   abstract findBySlug(slug: string): Promise<Question | null>
+  abstract findDetailsBySlug(slug: string): Promise<QuestionDetails | null>
   abstract findManyRecent(params: PaginationParams): Promise<Question[]>
   abstract create(question: Question): Promise<void>
   abstract save(question: Question): Promise<void>
